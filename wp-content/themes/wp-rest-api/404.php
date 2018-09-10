c61e7728d68d301bb7865c112f163d64c2cbb797
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-10 19:06:12 +0900

    Add new theme

---
+++ wp-content/themes/wp-rest-api/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php wp_head(); ?>
+</head>
+<body>
+  404 page not found...
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/wp-rest-api/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * remove tags
+ */
+
+remove_filter ('the_excerpt',  'wpautop');
+
+/**
+ * title
+ */
+function setup_theme() {
+  add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'setup_theme');
+
+function custom_title_separator() {
+  return ' | ';
+}
+add_filter('document_title_separator', 'custom_title_separator');
+
+function format_title($title) {
+  global $meta_title;
+  if (isset($title['tagline'])) {
+    unset($title['tagline']);
+  }
+
+  if (!empty($meta_title)) {
+    $title['title'] = $meta_title;
+  }
+  
+  return $title;
+}
+add_filter('document_title_parts', 'format_title');
+
+function get_page_title() {
+  $page_title = wp_get_document_title();
+  return $page_title;
+}
